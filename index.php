cda5c942d5ea596a3c83b2f8625bad29ba4934f2
richardly9319 <39727690+richardly9319@users.noreply.github.com>
2022-10-30 23:50:42 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form>
+        <input type="text" name="num1" placeholder="Number 1">
+        <input type="text" name="num2" placeholder="Number 2">
+        <select name="operator">
+            <option>None</option>
+            <option>Add</option>
+            <option>Subtract</option>
+            <option>Multiply</option>
+            <option>Divide</option>
+        </select>
+        <br>
+        <button name="submit" value="submit" type="submit">Calculate</button>
+    </form>
+    <p>Answer =  
+    <?php
+        if (isset($_GET['submit'])) {
+            $result1 = $_GET['num1'];
+            $result2 = $_GET['num2'];
+            $operator = $_GET['operator'];
+
+            switch ($operator) {
+                case "None":
+                    echo "Please select an operator";
+                break;
+                case "Add":
+                    echo $result1 + $result2;
+                break;
+                case "Subtract":
+                    echo $result1 - $result2;
+                break;
+                case "Multiply":
+                    echo $result1 * $result2;
+                break;
+                case "Divide":
+                    echo $result1 / $result2;
+                break;
+
+            }
+        }
+    ?>
+   
+    </p>
+
+</body>
+</html>
